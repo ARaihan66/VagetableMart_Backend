fd7210364bc4d7cbd219f0a4114755fb4f1f0ee7
Abu Raihan <aburaihancse066@gmail.com>
2025-12-08 12:53:06 +0600

    Connected database using mongodb atlas

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
 
 const app = express();
+const PORT = 8000 || process.env.PORT;
+
+await connectDB();
 
 const allowedOrigin = [""];
 // Middleware configuration
@@ -10,7 +15,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: allowedOrigin, credentials: true }));
 
-const PORT = 8000 || process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on the port ${PORT}`);
 });
