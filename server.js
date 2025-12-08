c9ad6ddb06fa94e38d6a24357a0296f1c4d3d7eb
Abu Raihan <aburaihancse066@gmail.com>
2025-12-08 12:15:28 +0600

    added middleware in server file

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+import cookieParser from "cookie-parser";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
-const PORT = 8000 || process.env.PORT;
+const allowedOrigin = [""];
+// Middleware configuration
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigin, credentials: true }));
 
+const PORT = 8000 || process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on the port ${PORT}`);
 });
