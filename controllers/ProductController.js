49b28e9795f0690bbc33f657cf7e06a020adfd21
Abu Raihan <aburaihancse066@gmail.com>
2025-12-09 11:16:28 +0600

    added product model

---
+++ controllers/ProductController.js
@@ -0,0 +1,11 @@
+// Add product
+export const addProduct = async (req, res) => {};
+
+// Get list of products
+export const productList = async (req, res) => {};
+
+// Get single product
+export const productById = async (req, res) => {};
+
+// Change inStock
+export const changeStock = async (req, res) => {};
